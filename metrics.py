b0b22ab7297290d4b785004e0ff6914bf3a86c1f
Abhijay <abhijaysingh.1011@gmail.com>
2022-08-21 12:42:02 +0530

    Modified metrics

--- metrics.py
+++ metrics.py
@@ -1,27 +1,59 @@
+##All the metrics used are derived from this file
 import math as m
+#side functions
+def derivative(x,y):
+    return x/y
+#Activation functions
+
 def sigmoid(x):
-    output = 1/(1+((1/m.e)**x))
+    output = 1/(1+(1/m.e)**x)
+    return output
+
+def tanh(x):
+    t1 = (m.e)**x
+    t2 = (1/m.e)**x
+    output = (t1 - t2)/(t1+t2)
     return output
 
 def relu(x):
     output = max(0,x)
     return output
 
+def leakyrelu(x):
+    output = max(0.01*x,x)
+    return output
+
+#Loss functions
+
+#For regression
+def MSE(yTrue,yPred):
+    loss = (yTrue-yPred)**2
+    return loss/2
+
+def MAE(yTrue,yPred):
+    loss = m.fabs(yTrue-yPred)/2
+    return loss
+
+def huberLoss(yTrue,yPred,delta):
+    if m.fabs(yTrue-yPred)<=delta:
+        loss = ((yTrue-yPred)**2)/2
+    else:
+        loss = delta*m.fabs(yTrue-yPred) - (delta**2)/2
+    
+    return loss
+
+#for classification
 
 def binaryCrossEntropy(yTrue,yPred):
     if yTrue==0:
-        loss = -1*(m.log(1-yPred + 0.0000001))
+        loss = -1*(m.log(1-yPred))
     elif yTrue==1:
-        loss = -1*(m.log(yPred+0.0000001))
+        loss = -1*(m.log(yPred))
     return loss
-    
-def derivative(loss,weight):
-    output = loss/weight
-    return output
 
-def accuracy(outputexpected,outputgiven):
-    diff = []
-    for z in range(len(outputexpected)):
-        diff.append(m.fabs(outputexpected[z]-outputgiven[z]))
-    accuracy = sum(diff)/len(diff)
-    return accuracy
+#Optimizers
+
+def gradientDescent(weight,loss,learningRate):
+    weight = weight - learningRate*(derivative(loss,weight))
+    return weight
+
